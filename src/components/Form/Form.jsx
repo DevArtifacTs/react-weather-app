dd3ddf9ed8c63119e12ab5d1e61cb721a9af6979
RaThomBoi <jessada.srm@gmail.com>
2022-06-08 23:05:51 +0700

    Component : initialize Form component with style module and function.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styles from './Form.module.css';
 
 function Form(props) {
+
+    const [location, setLocation] = React.useState('');
+
+    const handleChange = (e) => {
+        setLocation(e.target.value);
+    }
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!location || location === '') return;
+       
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit} className={styles.inputForm}  >
+            <input 
+                type="text" 
+                aria-label='location'
+                className={styles.input}
+                placeholder='Enter a location'
+                value={location}
+                onChange={handleChange}
+                required
+            />
+            <button 
+                type='submit' 
+                className={styles.button}
+                onClick={handleSubmit} 
+            >
+                SEARCH
+            </button>
+        </form>
     );
+
 }
 
 export default Form;
