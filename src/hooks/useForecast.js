4a454ba149372dd838a8fc8006dce22cf5a2e841
RaThomBoi <jessada.srm@gmail.com>
2022-06-08 23:55:12 +0700

    hooks : in useForecast hook add => submitRequest Fn with a mock up arg named 'location'

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -7,10 +7,16 @@ const useForecastHook = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [forecast, setForecast] = useState(false);
 
+    // call the api
+    const submitRequest = location => {
+        console.log({location});
+    }
+
     return {
         isError, 
         isLoading, 
         forecast,
+        submitRequest,
     }
 }
 
