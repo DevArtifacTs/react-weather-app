31cbc54a0630d050ec64ca763ec8f4df422ab4d6
RaThomBoi <jessada.srm@gmail.com>
2022-06-08 22:14:55 +0700

    Component : Implement Header component with header text and styles.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.css'
 
 function Header(props) {
     return (
-        <div>
-            
-        </div>
+        <header className="header">
+            <h1 className='header-text'>
+                Weather <span className='bold-header-text' >Forecast</span>
+            </h1>
+        </header>
     );
 }
 
