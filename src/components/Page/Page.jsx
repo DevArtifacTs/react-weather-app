d25975ea45e2a3c79cde28529b9d950d2d4f95bd
RaThomBoi <jessada.srm@gmail.com>
2022-06-08 23:05:16 +0700

    Comopnent : Page.jsx import components eg. Header, Form, Error, Loader, Forecast

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -7,11 +7,24 @@ import Error from '../Error/Error';
 import Loader from '../Loader/Loader';
 import Forecast from '../Forecast/Forecast';
 
+//style
+import styles from './Page.module.css';
 
 function Page(props) {
     return (
         <>
          <Header />
+         <div className={styles.box}>
+             {/* Form */}
+             <Form />
+             {/* Error */}
+             <Error />
+             {/* Loader */}
+             <Loader />
+             {/* Forecast */}
+             <Forecast />
+
+         </div>
 
         </>
     );
