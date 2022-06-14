035228b8fe7d95079ef80125ad5ab578477d5e7c
RaThomBoi <jessada.srm@gmail.com>
2022-06-14 22:16:30 +0700

    refactor && feat : refactor useForecast,js and implement api key in .env to const API_KEY on line 9

--- src/helpers/getCurrentForecast.js
+++ src/helpers/getCurrentForecast.js
@@ -1,17 +1,17 @@
 import moment from 'moment';
 
 const getCurrentForecast = (data, location ) => ({
-            weekday : moment(data.dt).format('dddd'),
-            date : moment(data.dt).format("MMM Do YY"),
-            location : location,
-            temp : Math.round(data.main.temp),
-            // tempMax : Math.round(data.main.temp_max),
-            // tempMin : Math.round(data.main.temp_min),
-            humid : data.main.humidity,
-            feelLike : data.main.feels_like,
-            windSpeed : data.wind.speed,
-            weatherIcon : `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-            weatherDescription : data.weather[0].description,
+    weekday : moment(data.dt).format('dddd'),
+    date : moment(data.dt).format("MMM Do YY"),
+    location : location,
+    temp : Math.round(data.main.temp),
+    // tempMax : Math.round(data.main.temp_max),
+    // tempMin : Math.round(data.main.temp_min),
+    humid : data.main.humidity,
+    feelLike : data.main.feels_like,
+    windSpeed : data.wind.speed,
+    weatherIcon : `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+    weatherDescription : data.weather[0].description,
  })
 
  export default getCurrentForecast;
--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -7,7 +7,7 @@ import getCurrentForecast from "../helpers/getCurrentForecast";
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall';
 const ALTER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const LAT_LONG_URL = 'http://api.openweathermap.org/geo/1.0/direct';
-const API_KEY = 'cde3993cbe012d67a44a7518e13b172c'
+const API_KEY = process.env.REACT_APP_API_KEY
 const REQUEST_URL =`${BASE_URL}`;
 
 const useForecastHook = () => {
@@ -18,40 +18,12 @@ const useForecastHook = () => {
     const [forecast, setForecast] = useState(false);
     const [ErrorResetButton, setErrorResetButton] = useState(false);
     
-    //1.get lat and long by enter city name 
-    // const getLatLong = async (cityName) => {
-    //     const {data} = await axios.get(`${LAT_LONG_URL}?q=${cityName}&limit=5&appid=${API_KEY}`);
-        
-    //     if(!data || data.length === 0){
-    //         setIsError('There is no such a location')
-    //     }
-    //     return {    lat: data[0].lat, 
-    //                 lon: data[0].lon
-    //             };
-    // }
-
-    //2.get forecast by lat and long
-    // const getForecast = async (lat, lon, exclude) => {
-    //     const {data} = await axios.get(`${BASE_URL}`, {params : {lat : lat, lon : lon, exclude : exclude, units : 'metric', appid : API_KEY}});
-    //     console.log('foreast: ', data);
-    //     return data;
-    // }
-
-    //extract for current, next 4 hours
-
     // gather all data and return
     const gatherForecastData =  (data, location) => {
         // current weather
         const current =  getCurrentForecast(data, location);
-        // console.log('current: ', current);
         return current
     }
-
-
-    // const getCurrentDataForecast = async (lat, lon) => {
-    //     const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    //     return data;
-    // }
 
     const getCurrentForecastByName = async city => {
         let response
@@ -78,28 +50,16 @@ const useForecastHook = () => {
         setIsLoading(true);
         setIsError(false);
 
-        // const {lat, lon} = await getLatLong(location);
-        // console.log ('location: ', location, 'lat: ', lat, 'lon: ', lon);
-
-        // const currentForecast = await getForecast(lat, lon, 'current');
-        // const currentDataForecast = await getCurrentDataForecast(lat, lon);
         const currentDataForecast = await getCurrentForecastByName(location);
-        // console.log('handle error', currentDataForecast.message);
-        // console.log('currentDataForecast: ', currentDataForecast);
-
         if(currentDataForecast === undefined){
             console.log('there is no currentDataForecast')
             setIsLoading(false);
 
         }
-
-        // gatherForecastData(currentForecast, location)
         setForecast( gatherForecastData(currentDataForecast, location))
         setIsLoading(false);
-
-        
+  
     }
-
 
     return {
         isError, 
